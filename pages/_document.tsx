8e2ccc87f0c4edb34fc37ec4673bb5906de50734
yashar habibi <habibi4webdesign@gmail.com>
2023-03-25 10:02:34 +0100

    raleway fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,16 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Hi, I'm Yashar Habibi. I work as a software engineer and specialize in front-end development. Although I was born in India, I grew up in Iran, and currently, I reside in Sweden."/>
-        <meta name="keywords" content="web developer, designer, Yashar Habibi, Habibikhamemenh, frontend, front-end, senior front end enginner"/>
-        <meta name="language" content="en"/>
-        <meta name="author" content="Yashar Habibi"/>
+        <meta
+          name="description"
+          content="Hi, I'm Yashar Habibi. I work as a software engineer and specialize in front-end development. Although I was born in India, I grew up in Iran, and currently, I reside in Sweden."
+        />
+        <meta
+          name="keywords"
+          content="web developer, designer, Yashar Habibi, Habibikhamemenh, frontend, front-end, senior front end enginner"
+        />
+        <meta name="language" content="en" />
+        <meta name="author" content="Yashar Habibi" />
 
         <link
           rel="stylesheet"
@@ -17,6 +23,12 @@ export default function Document() {
         />
         <link
           href="http://fonts.cdnfonts.com/css/raleway-5"
+          rel="stylesheet"
+        ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
       </Head>
