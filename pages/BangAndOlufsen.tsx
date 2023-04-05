6e7ee33833845a72190de78eb4b71a8809e0e320
yashar habibi <habibi4webdesign@gmail.com>
2023-04-05 13:39:25 +0200

    just for test

---
+++ pages/BangAndOlufsen.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './InstagramFeed.css';
+
+const InstagramFeed = () => {
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchInstagramPosts();
+  }, []);
+
+  const fetchInstagramPosts = async () => {
+    const token = 'IGQVJWVmdCbjFWazNOYlBlazRFZAmthbzJGNDJ3YWJIWEpDY09pWkdQanhDMTdNQU14Y0xaU2JtVGdHUk9rcko4Wm1BYUtsaG9nWko1OW5LSTRSUkxnVy1zYzVCaEpWN19nV2o4dHZAFZAHBuOWxPS1hWZAQZDZD';
+    const limit = 8;
+    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${token}&limit=${limit}`;
+
+    try {
+      const response = await axios.get(url);
+      setPosts(response.data.data);
+    } catch (error) {
+      console.error('Error fetching Instagram posts:', error);
+    }
+  };
+
+  return (
+    <div className="instagram-feed">
+      {posts.map((post, index) => (
+        <a key={index} href={post.permalink} target="_blank" rel="noopener noreferrer">
+          <img src={post.thumbnail_url || post.media_url} alt={post.caption} />
+        </a>
+      ))}
+      <a
+        className="instagram-view-more"
+        href="https://www.instagram.com/bang-olufsen"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        View more on Instagram
+      </a>
+    </div>
+  );
+};
+
+export default InstagramFeed;
